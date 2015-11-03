345f3f1392afc8ee412c2d56e931a9a3b6a4de04
Jeff Rose <jeff@thinktopic.com>
2015-11-03 13:48:51 -0700

    updating dependency versions

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject thinktopic/matrix.fressian "0.1.0"
+(defproject thinktopic/matrix.fressian "0.2.0"
   :description "Adds Fressian support for core.matrix."
   :url "http://github.com/rosejn/matrix.fressian"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [net.mikera/core.matrix "0.32.1"]
-                 [org.clojure/data.fressian "0.2.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [net.mikera/core.matrix "0.43.0"]
+                 [org.clojure/data.fressian "0.2.1"]]
 
-  :profiles {:dev {:dependencies [[net.mikera/vectorz-clj "0.28.0"]]}}
+  :profiles {:dev {:dependencies [[net.mikera/vectorz-clj "0.37.0"]]}}
 
   :lein-release {:deploy-via :clojars}
   )
