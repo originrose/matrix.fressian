76d86d48c1df6d9039e87aef59116e0fb79a201a
Jeff Rose <rosejn@gmail.com>
2016-03-07 12:16:38 +0000

    changing the matrix read strategy for cljs

--- src/thinktopic/matrix/fressian.cljs
+++ src/thinktopic/matrix/fressian.cljs
@@ -5,8 +5,6 @@
             [fressian-cljs.writer :as fwriter]
             [thinktopic.aljabr.core]))
 
-(enable-console-print!)
-
 (def ARRAY-TAG "array")
 
 (defn array-write-handler
@@ -20,15 +18,14 @@
 
 (defn array-read-handler
   [reader tag component-count]
-  (let [shape (freader/read-object reader)]
-    (mat/compute-matrix shape (fn [& _] (freader/read-double reader)))))
+  (let [shape (freader/read-object reader)
+        size (apply * shape)
+        ary (mat/new-array shape)
+        data (thinktopic.aljabr.core/data ary)]
+    (dotimes [i size]
+      (aset data i (freader/read-double reader)))
+    ary))
 
-; All of the types supported by ndarray lib:
-; - :generic
-; - :uint8 :uint8-clamped :uint16 :uint32
-; - :int8 :int16 :int32
-; - :float32 :float64
-; Each type gets 4 type definitions.
 (def WRITE-HANDLERS
  {thinktopic.aljabr.core/NDArray1float64 {ARRAY-TAG array-write-handler}
   thinktopic.aljabr.core/NDArray2float64 {ARRAY-TAG array-write-handler}
